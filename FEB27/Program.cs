07bffc2dbb83ebd7e7ab22249c6e02066371fcb8
visualstudio <visualstudio@122-PC16>
2019-02-27 09:08:50 -0500

    Commit1

--- FEB27/Program.cs
+++ FEB27/Program.cs
@@ -8,8 +8,12 @@ namespace FEB27
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            var orderProcessor = new OrderProcessor();
+            var order = new Order { DatePlaced = DateTime.Now,TotalPrice = 100f };
+            orderProcessor.Process();
         }
     }
 }
