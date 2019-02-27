557a007b3435937aa5ebcbedf20634ff6cdc9ec1
visualstudio <visualstudio@122-PC16>
2019-02-27 09:56:59 -0500

    Commit12

--- FEB27/Program.cs
+++ FEB27/Program.cs
@@ -13,7 +13,34 @@ namespace FEB27
         {
             var orderProcessor = new OrderProcessor();
             var order = new Order { DatePlaced = DateTime.Now,TotalPrice = 100f };
-            orderProcessor.Process();
+            orderProcessor.Process(order);
         }
+    }
+}
+public class OrderProcessor
+{
+    private readonly ShippingCalculator _shippingCalculator;
+       public OrderProcessor()
+     {
+        _shippingCalculator = new ShippingCalculator();
+    }
+    public void Process(Order order)
+    {
+        if (order.IsShipped)
+            throw new InvalidOperationalException("This order is already Shipped");
+        order.shipment = new shipment
+        {
+            Cost = _shippingCalculator.Calculateshipping(order),
+            ShippingDate = DateTime.Today.AddDays(1)
+        };
+    }
+
+}
+public class ShippingCalculator
+{
+    public float CalculateShipping(Order order)
+    {
+        if (order.TotalPrice < 30f) return order.totalPrice * 0.1f;
+        return 0;
     }
 }
